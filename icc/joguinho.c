155275858c75b3ded2c261e82ad99b13bbc42c78
Henrique Hiram Libutti Núñez <32100626+henriquenunez@users.noreply.github.com>
2019-04-22 19:15:00 -0300

    Create joguinho.c

---
+++ icc/joguinho.c
@@ -0,0 +1,24 @@
+#include "stdlib.h"
+
+void generate_values(int num_seq , int num_values , int time , int *score){
+
+}
+
+int main(){
+    int seed;
+    char option;
+    scanf("%d" , &seed);
+    srand(seed);
+    scanf("%c" , &option);
+    int *total = 0;
+
+    swtich(option){
+        case 'F':
+            generate_values(3 , 5 , 5 , total);
+        case 'M':
+            //coisas
+        case 'A':
+            //coisas
+
+    }
+}
