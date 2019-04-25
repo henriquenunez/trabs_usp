516c5f1b0b85addc974143d92e7d32bba5f05827
Henrique Hiram Libutti Núñez <32100626+henriquenunez@users.noreply.github.com>
2019-04-25 14:42:08 -0300

    Versão *quase* pronta
    100% atualizado livre de virus 2019 crackead alissondigital@hotmail.com

--- icc/joguinho.c
+++ icc/joguinho.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int score(){
-
+void score(int num_values , int errors , int *total , int *actual){
+  
+  int max = 10 * num_values;
+  
+  return;
 }
 
 int check_values(int num_values , int *seq){
@@ -13,15 +16,21 @@ int check_values(int num_values , int *seq){
   
   for(index_val = 0 ; index_val < num_values; index_val++){
     scanf("%d" , &answer[index_val]);
-    printf("to ak\n");
-    if ( seq[index_val] == answer[index_val] ){
-      printf("acerto!!!!! mizeravi\n");
-    } else {
+    if ( !(seq[index_val] == answer[index_val]) ){
       err++;
-      printf("Errou!!!!! fdp\n");
     }     
   }
   return err;
+}
+
+void disp_values(int num_values , int *seq){
+ 
+  int index_val;
+  
+  for(index_val = 0 ; index_val < num_values; index_val++){
+    printf("%d " , seq[index_val]);
+  }
+  printf("\n");  
 }
 
 void generate_values(int num_values , int *seq){
@@ -30,47 +39,66 @@ void generate_values(int num_values , int *seq){
   
   for(index_val = 0 ; index_val < num_values; index_val++){
     seq[index_val] = rand() % 10;
-    printf("%d " , seq[index_val]);
   }
-  
-  printf("\n");  
 }
 
 int main(){
-    int seed;
-    int index_seq;
-    int num_seq;
-    int num_val;
-    int sleep_time;
-    int *total = 0;
-    int sequence[9];
-    char option;
-    
-    
-    scanf("%d %c" , &seed , &option);
-    srand(seed);
-    
 
-    switch(option){
-      case 'F':
-        num_seq = 3;
-        num_val = 5;
-        sleep_time = 5;
-        break;
-      case 'M':
-        num_seq = 5;
-        num_val = 5;
-        sleep_time = 5;
-        break;
-      case 'A':
-        num_seq = 7;
-        num_val = 5;
-        sleep_time = 5;
-        break;
-    }
+  int seed;
+  char option;
 
-    for(index_seq = 0 ; index_seq < num_seq; index_seq++){
-      generate_values(num_val, &sequence[0]);
-      check_values(num_val , &sequence[0]);
+  int index_seq;
+  int num_seq;
+  int num_val;
+  
+  int sleep_time;
+  int *total = 0;
+  int sequence[9];
+  
+  int trials = 3;
+  
+  scanf("%d %c" , &seed , &option);
+  srand(seed);
+  
+
+  switch(option){
+    case 'F':
+      num_seq = 3;
+      num_val = 5;
+      sleep_time = 5;
+      break;
+    case 'M':
+      num_seq = 5;
+      num_val = 5;
+      sleep_time = 5;
+      break;
+    case 'A':
+      num_seq = 7;
+      num_val = 5;
+      sleep_time = 5;
+      break;
+  }
+
+  for(index_seq = 0 ; index_seq < num_seq; index_seq++){
+
+    generate_values(num_val, &sequence[0]);
+
+    while(trials){
+      printf("Sequencia #%d:\n" , index_seq + 1 );
+      disp_values(num_val, &sequence[0]);
+      if( check_values(num_val , &sequence[0]) ){
+        trials--;
+        printf("Erou!\n");
+      } else {
+        printf("Ah mizeravi!\n");
+        break;
+      }
     }
+    printf("TU PODE FAZER %d\n" , trials);
+    //TODO printf("TANTOS PONTOS O KRLL %d\n" , *total);
+  }
+  
+  if(trials){
+    printf("BOM DMS ZIKA!\n");
+  }
 }
