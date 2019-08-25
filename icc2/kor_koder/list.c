16484b2891f445b1bf98b0b33083aa1bdeaddd41
hiram <hiram@argentum>
2019-08-25 18:37:50 -0300

    pau no cu do run codes

--- icc2/kor_koder/list.c
+++ icc2/kor_koder/list.c
@@ -9,55 +9,111 @@ struct _node {
 };
 
 NODE* newNode(int data);
-void insertAtSmaller(NODE** head, int data);
+NODE* searchSmaller(NODE* head, int value);
+void insertAtNode(NODE* node, int data);
 void printList(NODE* list);
-//NODE* searchSmaller(NODE* list, int value);
+void push(NODE** head, NODE* node);
+void insertSmaller(NODE** head, int value);
+//void push(NODE** head, NODE* node);
+//NODE* searchSmaller(NODE** head, int value);
 
 int main(){
 	int num, input;
-	NODE* head;
-	NODE* temp;
+	NODE* head = NULL;
 
 	scanf("%d", &num);
+//	fprintf(stderr, "%d", num);
 	for (int i = 0; i < num; i++)
 	{	
 		scanf("%d", &input);
-		insertAtSmaller(&head, input);
+		//fprintf(stderr, "%d", input);		
+		insertSmaller(&head, input);
 	}
+
 	printList(head);
 }
 
-void insertAtSmaller(NODE** head, int data){
-	NODE* new;
-	NODE* current;
-	NODE* previous;
-	new = (NODE*) malloc(sizeof(NODE));
-
+NODE* newNode(int data){
+	NODE* new = (NODE*) malloc(sizeof(NODE));
 	new->content = data;
+	
+	if(new != NULL)
+		return new;
+	else
+		return NULL;
+}
 
-	if (*head == NULL){
-		*head = new;
+void push(NODE** head, NODE* node){
+	node->next = (*head);
+	(*head) = node;
+}
+
+void insertAtNode(NODE* node, int data){
+	NODE* new = newNode(data);
+	
+	if(node->next != NULL)
+		new->next = node->next;
+	node->next = new;
+	
+	return;
+}
+
+void insertSmaller(NODE** head, int value){
+	//NODE* temp = (*head);
+	
+	if(*head == NULL){
+		//printf("head was null\n");
+		*head = newNode(value);
 		return;
 	}
 
-	current = *head;
-	previous = current;
+	if((*head)->content >= value){
+		//printf("head was greater\n");
+		push(head, newNode(value));
+		return;
+	}
+/*	
+	if((*head)->content < value){
+		insertAtNode(*head, value);
+		return;
+	}
+*/
+	insertAtNode(searchSmaller((*head), value), value);
+}
 
-	while(current != NULL){
-		if(current->content > data){
-			new->next = current;
-			previous->next = new;
+
+NODE* searchSmaller(NODE* head, int value){
+	NODE* temp = head;
+	
+//	while(temp->next != NULL)
+	while(1){
+		if (temp->next != NULL){
+			if(temp->content < value && temp->next->content > value){
+				//printf("less than val");
+				return temp;
+			}
+			temp = temp->next;	
+		}else{
+			if(temp->content < value){
+				return temp;
+			}
 		}
 	}
 	
+	return NULL;
 }
 
 void printList(NODE* head){
 	NODE* temp;
 	temp = head;
+
 	while(temp != NULL){
-		printf("content: %d\n", temp->content);
+		printf("%d", temp->content);
 		temp = temp->next;
+		if(temp == NULL){
+			printf("\n");
+		}else{
+			printf(" ");
+		}
 	}
 }
-
