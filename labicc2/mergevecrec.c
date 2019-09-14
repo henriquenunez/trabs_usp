03bdcc567a95138acf5840a4e8c1ef8e24cbf68a
Henrique Hiram Libutti Nunez <USUARIOS+11275300@6-304-09.grad.usricmc.icmc.usp.br>
2019-09-14 13:56:21 -0300

    sla

--- labicc2/mergevecrec.c
+++ labicc2/mergevecrec.c
@@ -1,28 +1,34 @@
 #include <stdlib.h>
-#include <stdool.h>
+#include <stdbool.h>
 #include "mergevec.h"
 
-void recursiveMerge(int count, int vecA[], int vecB[], int newVec[] ,bool op);
+#define VEC_SEL            1  // 2^0, bit 0
+#define VEC_A_FINISHED     2  // 2^1, bit 1
+#define VEC_B_FINISHED     4  // 2^2, bit 2
 
-mergeVecs()
+void recursiveMerge(int count, int vecA[], int vecB[], int newVec[], unsigned char op, int max);
+
+int* mergeVecs(int vecA[], int vecB[], int sizeA, int sizeB)
 {
-    int* newVec = (int*) calloc (NUM_ELE, NUM_ELE*sizeof(int));
+    #define NUM_ELE sizeA + sizeB
+    int* newVec = (int*) calloc (NUM_ELE, NUM_ELE * sizeof(int));
 
-    recursiveMerge(0, vecA, vecB, newVec, 0);
+    recursiveMerge(0, vecA, vecB, newVec, 0, NUM_ELE);
 
     return newVec;
 }
 
-recursiveMerge()
+void recursiveMerge(int count, int vecA[], int vecB[], int newVec[], unsigned char op, int max)
 {
-    if (op)
+    if (count >= max) return;
+    if ( ( ( (op) & VEC_SEL ) | VEC_A_FINISHED) & ~VEC_B_FINISHED)
     {
-        newVec[i] = vecA[(int)(i/2)];
-        recursiveMerge(count+1, 0)
+        newVec[count] = vecB[(int)(count/2)];
+        recursiveMerge(count+1, vecA, vecB, newVec, op ^ VEC_SEL, max);
     }
     else
     {
-        newVec[i] = vecA[(int)(i/2)];
-        recursiveMerge(count+1, 1)
+        newVec[count] = vecA[(int)(count/2)];
+        recursiveMerge(count+1, vecA, vecB, newVec, op ^ VEC_SEL, max);
     }
 }
